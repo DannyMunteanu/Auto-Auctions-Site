91c59c242f3af0b44d22b4e52f974580b6e085f1
Daniel <120681842+grngoo@users.noreply.github.com>
2024-08-19 17:58:31 +0100

    Initial project commit

---
+++ Backend/src/main/java/com/github/grngoo/AutoAuctions/AutoAuctionsApplication.java
@@ -0,0 +1,13 @@
+package com.github.grngoo.AutoAuctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoAuctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoAuctionsApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/github/grngoo/AutoAuctions/AutoAuctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.grngoo.AutoAuctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoAuctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
